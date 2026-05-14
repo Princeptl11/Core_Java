84573e345b2fb4a2eb45d0dcf2d69df8976e6f79
Prince Patel <princeptl099@gmail.com>
2026-05-14 10:19:20 +0530

    practice code

--- Basics/Keywords/practice1.java
+++ Basics/Keywords/practice1.java
@@ -2,7 +2,7 @@ class practice1 {
     public static void main(String[] args) {
         for(int i = 1; i <= 5; i++) {
             for(int j = 1; j <= 5; j++) {
-                if(i==1 || j==1) {
+                if(i==1 || j==1 || i==5 || j==5) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");   //
